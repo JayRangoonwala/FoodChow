e12c8cb0a4995f5056f4ec318ab387e4728ee918
Pratik Chauhan <android@tenacioustechies.com>
2025-04-30 12:34:29 +0530

    wildcard subdomain support

--- lib/getSubdomain.ts
+++ lib/getSubdomain.ts
@@ -5,7 +5,7 @@ import { headers } from "next/headers";
 import { cache } from "react";
 
 export const getSubdomainFromHeaders = cache(async () => {
-  /*const headersList = await headers();
+  const headersList = await headers();
   const host = headersList.get("host") || "";
 
   if (host === "foodchow.in") {
@@ -15,7 +15,7 @@ export const getSubdomainFromHeaders = cache(async () => {
   const parts = host.split(".");
   if (parts.length > 2) {
     return parts[0] === "www" ? parts[1] : parts[0];
-  }*/
+  }
 
   return "foodchowdemoindia"; // fallback
 });
